0809d103182b577bfdc868625258aba59f61ccd0
gnomgad <canimore@yandex.ua>
2021-10-14 23:37:09 +0300

    добавил аргпарсер и доработал main@

--- main.py
+++ main.py
@@ -1,42 +1,63 @@
+import argparse
 import os.path
 import collections
+import os
+from brute_force import BruteForce
 from symbol_analyzer import SymbolAnalyzer
 from app import get_file_data, write_data
-path_lab1 = '../information-protection-lab-1'
-sa = SymbolAnalyzer()
 
-def exp_1(): #не сработало
-    data = get_file_data('e.data')
-    # два алфавита
-    freq_alp = sa.get_alphabet(sa.read('freq.json'))
-    enc_freq = sa.get_alphabet(sa.get_frequency_analyse(sa.get_symbols_frequency(data)))
-    new_list = []
-    for c in enc_freq:
-        if c in freq_alp:
-            new_list.append(c)
-    new_data = data
-    for i in range(len(freq_alp)):
-        new_data = new_data.replace(freq_alp[i],new_list[i])
-    write_data('out.txt', new_data)
 
-    print(freq_alp,'\n')
-    print(new_list)
+def main(args):
+    cores = 2 if not args.cores else args.cores
+    krange = [1,1]
+    bf = None
+    verify = None
 
+    # Если нету вообще ничего
+    if not args.count and not args.range:
+        raise Exception('Необходимо указать диапазон' )
+
+    # Если есть ренж
+    elif args.range:
+        krange = args.range.split(':')
+        if len(krange) != 2 or not (krange[0].isdigit() and krange[1].isdigit()):
+            raise Exception('Диапазон указан неверно' )
+        krange[0] = int(krange[0])
+        krange[1] = int(krange[1])
+    # Если нет ренжа
+    else :
+        krange[1] = abs(args.count)
+
+    # Если нет папки то создать
+    if not os.path.exists(args.dir):
+        os.makedirs(args.dir)
+
+    # Если есть bf и путь с файлом
+    if args.bf and os.path.exists(args.bf):
+        if not args.encrypted or not os.path.exists(args.encrypted):
+             raise Exception('Нет зашифрованого файла' )
+        bf = BruteForce(cores, krange, args.bf, args.encrypted, args.dir)
+        print('Секунд ', bf.execute())
+    
+    # TODO: реализовать анализатор текста
+
+
+
+# Путь с декодированием
+# Путь к папке с app.py
+# Путь к файлу со словами для проверки
+# Количество проходов
+# python main.py  /home/odin/Source/information-protection-lab-2/ex -b /home/odin/Source/information-protection-lab-1/app.py -e /home/odin/Source/information-protection-lab-1/o.txt  -v words.txt -r 100:500 -C 16
 
 if __name__ == "__main__":
-    data = get_file_data('e.data')
-    # два алфавита
-    freq_alp = sa.get_alphabet(sa.read('freq.json'))
-    enc_freq = sa.get_alphabet(sa.get_frequency_analyse(sa.get_symbols_frequency(data)))
-    new_list = []
-    for c in freq_alp:
-        if c in enc_freq:
-            new_list.append(c)
-    new_data = data
-    for i in range(len(new_list)):
-        new_data = new_data.replace(freq_alp[i],new_list[i])
-    write_data('out.txt', new_data)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dir', type=str, help='Путь к папке с результатом для брутфосом')
+    parser.add_argument('-b', '--brute', dest='bf', help='Если нужен брутфорс, то необходимо указать путь к  app.py из первой лабы')
+    parser.add_argument('-e', '--encrypted', dest='encrypted', help='Файл для расшифроки')
+    parser.add_argument('-v', '--verify', dest='verify', help='Если нужно проверить результат брутфорса, то необходимо указать путь к файлу со словами')
+    parser.add_argument('-r','--range', dest='range',type=str, help='Диапазон в формате 1000:5000')
+    parser.add_argument('-c', '--count', dest='count',type=int,  help='Диапазон от 0 до count')
+    parser.add_argument('-C','--cores', dest='cores', type=int, help='Количество ядер процессора, что участвуют в процессе max или количество')
+    args = parser.parse_args()
 
-    print(freq_alp,'\n')
-    print(new_list)
-    
+    main(args)
