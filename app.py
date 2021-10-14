39ec0d41875bc91557b1d43cf12061514fe5b020
gnomgad <canimore@yandex.ua>
2021-10-14 23:37:30 +0300

    другие изменения

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import multiprocessing
 
 def write_data(file_path, text, encoding='utf-8'):
     """Записать данные в файл."""
-    with open(file_path, 'w') as fp:
+    with open(file_path, 'w+') as fp:
         fp.write(text)
 
 
@@ -21,26 +21,3 @@ def get_file_data(file_path):
         data = fp.read()
 
     return data
-
-def if_prime(x):
-    if x <= 1:
-        return 0
-    elif x <= 3:
-        return x
-    elif x % 2 == 0 or x % 3 == 0:
-        return 0
-    i = 5
-    while i**2 <= x:
-        if x % i == 0 or x % (i + 2) == 0:
-            return 0
-        i += 6
-    return x
-
-if __name__ == '__main__':
-    answer =0
-    PROCESSORS = multiprocessing.cpu_count()
-    start_time = time.time()
-    with Pool(PROCESSORS) as p:
-        answer = sum(p.map(if_prime, list(range(1000000))))
-    print(answer)
-    print('Время выполнения:',time.time() - start_time, 'секунд')
--- text_analyser.py
+++ text_analyser.py
@@ -1,24 +1,45 @@
-import collections
-import json
-from collections import Counter 
 
+import time 
+import subprocess
+from subprocess import Popen, PIPE
+from multiprocessing import Process, Queue, Pool
+from app import get_file_data
+import multiprocessing
+import tqdm
 
-def file_frequency_analyse(file_path:str) -> dict:
-    """Прочитать целиком файл по пути."""
-    data = ''
-    with open(file_path, 'r', encoding='utf-8') as fp:
-        data = fp.read()
-    return frequency_analyse(data)
+from app import write_data, get_file_data
+path_lab1 = '/home/odin/Source/information-protection-lab-1/'
 
-def frequency_analyse(data:str) -> dict:
-    """Функция реализующая подсчет """
-    d = collections.Counter() #collections.defaultdict(int)
-    d = Counter(data) 
-    l = len(data)
-    d = dict(d)
-    with open('f.json', 'w',encoding='utf-8') as f:
-        json.dump(d,f,ensure_ascii=False)
+"""Смысл в прямом подборе пароля. Работает только если пароль  x < 5 знаков"""
+
+def check(i):
+    s = get_file_data('{0}ex/{1}.txt'.format(path_lab1, i))
+    d = s.split(' ')
+    new_lst = lst_words
+    for word in d:
+        word = word.lower()
+        if  word in new_lst:
+            new_lst.remove(word)
+            if not new_lst:
+                return i
+keys = []
+allProcesses = []
+
+lst_words = 'что и в не я хочешь'.split(' ')
+
 
 
 if __name__ == '__main__':
-    file_frequency_analyse('d.data')
+    lenB = 1000
+    PROCESSORS = multiprocessing.cpu_count()
+    start_time = time.time()
+    res = []
+
+    # TODO: перенести в отдельную функцию поиск
+    with Pool(PROCESSORS) as p:
+        res = list(tqdm.tqdm(p.imap(check, list(range(lenB))),total=lenB))
+        for r in res:
+            if r:
+                print("Предположительный ключ:", r)
+    print('Время выполнения:',time.time() - start_time, 'секунд')
+    print('Все файлы находятся в {0}\\ex\\'.format(path_lab1))
